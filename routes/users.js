69d37bb0a4710b71d4972ad352c7120ed59e4330
nrsmsf <1nrsmsf@gmail.com>
2023-04-07 02:05:12 +0400

    feat: more routes

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,33 @@
 const express = require("express")
 const router = express.Router()
-router.get("/", (
-    req,
-    res) =>
+router.get("/", (req, res) => {
     res.send("User list")
-)
-router.get("/new", (
-    req,
-    res) =>
+})
+router.get("/new", (req, res) => {
     res.send("User New form")
-)
+})
+
+// dynamic routes
+router.route("/:id")
+    .get((req, res) => {
+        console.log(req.user)
+        res.send(`get user with id ${req.params.id}`)
+    })
+    .post((req, res) => {
+        res.send(`post user with id ${req.params.id}`)
+    })
+    .put((req, res) => {
+        res.send(`put user with id ${req.params.id}`)
+    })
+    .delete((req, res) => {
+        res.send(`delete user with id ${req.params.id}`)
+    })
+
+
+const users = [{name: "Kek"}, {name: "Lol"}]
+router.param("id", (req, res, next, id) => {
+    req.user = users[id]
+    next()
+})
 
 module.exports = router
